c6963c8c03a3c84750b2bf4b1c5cabb77747dec1
harry0890 <eternity050890@gmail.com>
2019-11-15 17:54:20 +0800

    Add files via upload

--- data-structure/offsetof.c
+++ data-structure/offsetof.c
@@ -4,16 +4,18 @@
 #define my_offsetof(TYPE, MEMBER)  ((size_t) &((TYPE *)1000)->MEMBER)
 
 struct MY_DATA {
-	int Data_A;
-	int Data_B;
-	int Data_C;
-	struct MY_DATA *next;
+	int Data_A; // offset = 0
+	int Data_B; // offset = 4
+	int Data_C; // offset = 8
+	struct MY_DATA *next; // offset = 16 ( 8 + 8(x86 pointer))
 };
 
 typedef struct MY_DATA AAA;
 
 int main(void)
 {
+	int *ptr;
+
 	puts("----- offsetof -----");
 	printf("---- next  =  %ld-----\n", offsetof(struct MY_DATA, next));
 	printf("---- Data_A =  %ld -----\n", offsetof(struct MY_DATA,Data_A));
@@ -21,5 +23,8 @@ int main(void)
 	printf("---- Data_C =  %ld -----\n", offsetof(struct MY_DATA,Data_C));	
 	puts("\n---- my_offsetof----");
 	printf("---- next  =  %ld----\n", my_offsetof(struct MY_DATA, next));
-	printf("MY_DATA sizeof = %ld\n", sizeof(struct MY_DATA));
+	printf("MY_DATA sizeof = %ld\n", sizeof(struct MY_DATA)); // 4*3 + 4*3 = 24
+	
+	printf("ptr = %ld\n", sizeof(ptr));  // pointer type = 8
+	printf("ptr type = %ld\n", sizeof(*ptr)); // pointer to int type = 4
 }
